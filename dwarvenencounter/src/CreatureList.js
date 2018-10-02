57221f099ef5b5b022428a323f229a71d016b5f2
zigzbox <glen.hecht@gmail.com>
2018-10-01 20:30:18 -0600

    v1.02 staring to work on the modals

--- dwarvenencounter/src/CreatureList.js
+++ dwarvenencounter/src/CreatureList.js
@@ -9,7 +9,25 @@ class CreatureList extends Component {
                 </header>
                 <main>
                     <body>
-                        
+                        <div>
+                            <h2>Artorias, the Abysswalker</h2>
+                            <ul>
+                                <li>STR:18(+4) DEX:18(+4) CON:18(+4) INT:14(+2) WIS:16(+3) CHA:12(+1)</li>
+                                <li><b>Saving Throws:</b>Str +10, Dex +10, Con +10</li>
+                                <li><b>Damage Immunities:</b>Necrotic, Poison</li>
+                                <li><b>Condition Immunities:</b> Charmed, Exhaustion, Frightened, Poisoned</li>
+                                <li><b>Senses:</b>Passive perception 13</li>
+                                <li><b>Languages:</b>Common, Abyssal</li>
+                                <li><b>Challenge:</b>17(18000 XP)</li>
+                                <li><b>Magic Resistance:</b>Artorias has advantage on saving throws against spells and other magical effects.</li>
+                                <li><b>Dark Weapon:</b>When Artorias hits with his weapon, the weapon deals an extra 4d8 necrotic damage (included in the attack).</li>
+                            </ul>
+                            <h3>Actions</h3>
+                            <ul>
+                                <li><b>Multiattack:</b>Artorias makes three greatsword attacks</li>
+                                <li><b>Greatsword:</b></li>
+                            </ul>
+                        </div>
                     </body>
                 </main>
             </div>
